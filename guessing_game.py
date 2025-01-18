7778ec67dc2f17475a5e006a3ded1dea9cc8c232
chadhindsight <chadrickJ8@gmail.com>
2025-01-18 10:50:11 -0500

    Initial setup of logic

--- guessing_game.py
+++ guessing_game.py
@@ -1,3 +1,14 @@
 player_choice = input("Guess a number between 1 and 10: ")
 
 # NB: randint is inclusive, not exclusive 
+computer_choice = randint(1, 10)
+keep_going = True
+
+while(keep_going == True):
+    if(player_choice == computer_choice):
+        print("Wow, you and the machine are on the same wavelength")
+        
+    elif(player_choice > computer_choice):
+        print("You guessed too high!")
+    else:
+        print("You guessed too low!")        
