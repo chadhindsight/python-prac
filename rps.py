8b5050315725181e9c14447b34d5f0df90ec27d8
chadhindsight <chadrickJ8@gmail.com>
2025-01-20 14:20:38 -0500

    let players quit the game early

--- rps.py
+++ rps.py
@@ -1,32 +1,38 @@
 from random import randint
+player_wins = 0
+computer_wins = 0
+win_score = 3
 
-# Computer choices
-player = input("Player, make your move: ").upper()
-rand_num = randint(0,2)
-if rand_num == 0:
-	computer = "rock"
-elif rand_num == 1:
-	computer = "paper"
-else:
-	computer = "scissors"
-print(f"Computer plays {computer}" )
+while player_wins < win_score and computer_wins < win_score:
+    # Computer choices
+	player = input("Player, make your move: ").lower()
+	if player == "q" or player == "quit":
+		break
+	rand_num = randint(0,2)
+	if rand_num == 0:
+		computer = "rock"
+	elif rand_num == 1:
+		computer = "paper"
+	else:
+		computer = "scissors"
+	print(f"Computer plays {computer}" )
 
-if player == computer:
-	print("It's a tie!")
-elif player == "rock":
-	if computer == "scissors":
-		print("player wins!")
+	if player == computer:
+		print("It's a tie!")
+	elif player == "rock":
+		if computer == "scissors":
+			print("player wins!")
+		else:
+			print("computer wins!")
+	elif player == "paper":
+		if computer == "rock":
+			print("player wins!")
+		else:
+			print("computer wins!")
+	elif player == "scissors":
+		if computer == "paper":
+			print("player wins!")
+		else:
+			print("computer wins!")	
 	else:
-		print("computer wins!")
-elif player == "paper":
-	if computer == "rock":
-		print("player wins!")
-	else:
-		print("computer wins!")
-elif player == "scissors":
-	if computer == "paper":
-		print("player wins!")
-	else:
-		print("computer wins!")	
-else:
-	print("Please enter a valid move!")
+		print("Please enter a valid move!")
