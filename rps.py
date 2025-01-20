c36c0467f98066253493b6ce0c8eba05e07d1849
chadhindsight <chadrickJ8@gmail.com>
2025-01-20 14:29:14 -0500

    finalize rps logic and keep track of scores

--- rps.py
+++ rps.py
@@ -4,6 +4,8 @@ computer_wins = 0
 win_score = 3
 
 while player_wins < win_score and computer_wins < win_score:
+	print(f"player Score: {player_wins} computer's score : {computer_wins}")
+
     # Computer choices
 	player = input("Player, make your move: ").lower()
 	if player == "q" or player == "quit":
@@ -16,23 +18,29 @@ while player_wins < win_score and computer_wins < win_score:
 	else:
 		computer = "scissors"
 	print(f"Computer plays {computer}" )
-
+	#  Player choices 
 	if player == computer:
 		print("It's a tie!")
 	elif player == "rock":
 		if computer == "scissors":
 			print("player wins!")
+			player_wins += 1
 		else:
 			print("computer wins!")
+			computer_wins += 1
 	elif player == "paper":
 		if computer == "rock":
 			print("player wins!")
+			player_wins += 1
 		else:
 			print("computer wins!")
+			computer_wins += 1
 	elif player == "scissors":
 		if computer == "paper":
 			print("player wins!")
+			player_wins += 1
 		else:
 			print("computer wins!")	
+			computer_wins += 1
 	else:
 		print("Please enter a valid move!")
