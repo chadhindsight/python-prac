40e327319009a30ec75911245dd151bdf8e8e3a2
chadhindsight <chadrickJ8@gmail.com>
2025-01-22 12:23:25 -0500

    Add new list specific examples

--- lists.py
+++ lists.py
@@ -5,5 +5,19 @@ result = ""
 
 for sound in sounds:
     result += sound.upper()
-      
 
+
+
+
+
+# Changing/Manipulating vals in lists
+
+# Add the following strings to the instructors list 
+    # "Colt"
+    # "Blue"
+    # "Lisa"
+
+# Run the tests to make sure you've done this correctly!
+instructors = []
+
+instructors.extend(["Colt", "Blue", "Lisa"])
