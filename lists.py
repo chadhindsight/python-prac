27815c8b91c0d7b9e9e981b27c04c5db9f48f111
chadhindsight <chadrickJ8@gmail.com>
2025-01-22 12:47:03 -0500

    Changing/Manipulating vals in lists

--- lists.py
+++ lists.py
@@ -11,13 +11,20 @@ for sound in sounds:
 
 
 # Changing/Manipulating vals in lists
-
+# Create a list called instructors
+instructors = []
+ 
 # Add the following strings to the instructors list 
     # "Colt"
     # "Blue"
     # "Lisa"
-
-# Run the tests to make sure you've done this correctly!
-instructors = []
-
 instructors.extend(["Colt", "Blue", "Lisa"])
+ 
+# Remove the last value in the list
+instructors.pop()
+ 
+# Remove the first value in the list
+instructors.pop(0)
+ 
+# Add the string "Done" to the beginning of the list
+instructors.insert(0, 'Done')
