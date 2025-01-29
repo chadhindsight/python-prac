f78a5e186aaac9ba5c2ce4176780ff831a4ddbed
chadhindsight <chadrickJ8@gmail.com>
2025-01-28 21:09:31 -0500

    the slice [::-1] is a quick way to reverse a string

--- lists.py
+++ lists.py
@@ -35,4 +35,8 @@ instructors.insert(0, 'Done')
 numbers = [34, 50, 78, 100, 60]
 
 doubleUp = [number * 2 for number in numbers]
-print(doubleUp)
+
+# Using list comprehensions(the more Pythonic way): 
+answer = [val for val in [1,2,3,4] if val in [3,4,5,6]]
+#the slice [::-1] is a quick way to reverse a string
+answer2 = [val[::-1].lower() for val in ["Elie", "Tim", "Matt"]]
