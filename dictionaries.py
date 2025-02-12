37c10272803e578b447ccdca4fd95445f24ccb1b
chadhindsight <chadrickJ8@gmail.com>
2025-02-12 11:13:09 -0500

    Add the value 18 to stock_list under a new key called "cookie"

--- dictionaries.py
+++ dictionaries.py
@@ -36,8 +36,7 @@ inventory = {'croissant': 19, 'bagel': 4, 'muffin': 8, 'cake': 1} #DON'T CHANGE 
 # Make a copy of inventory and save it to a variable called stock_list USE A DICTIONARY METHOD
 stock_list = inventory.copy()
 
-
-# add the value 18 to stock_list under the key "cookie"
+# add the value 18 to stock_list under a new key called "cookie"
 stock_list['cookie'] = 18
 
 
