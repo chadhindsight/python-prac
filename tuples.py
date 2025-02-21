0cb513790d1d7d42bbb952acc83b8fa354da3117
chadhindsight <chadrickJ8@gmail.com>
2025-02-21 12:30:43 -0500

    Add an example of looping through a Tuple

--- tuples.py
+++ tuples.py
@@ -1,2 +1,9 @@
 # NB: Tuples use () instead of []
+
 # NB: Tuples are faster than Lists and are immutable
+
+# Looping in a Tuple
+names = ('Koya', 'Dollie', 'Devin')
+
+for name in names:
+    print(name)
