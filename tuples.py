2ba2260388915722a7d4b494612a495a80bd8344
chadhindsight <chadrickJ8@gmail.com>
2025-02-26 14:12:34 -0500

    Add an example of the two tuple methods

--- tuples.py
+++ tuples.py
@@ -7,3 +7,9 @@ names = ('Koya', 'Dollie', 'Devin')
 
 for name in names:
     print(name)
+
+# Tuple Methods(count & index)
+t = (12, 5, 9, 8, 12)
+t.index(1) # returns the index at which a value is found in a tuple
+
+t.count(12) # returns the number of times a value appears in a tuple
