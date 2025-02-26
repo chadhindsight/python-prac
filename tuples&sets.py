9f646517cd23596a6b29440a7bddeda889ed1b62
chadhindsight <chadrickJ8@gmail.com>
2025-02-26 14:15:45 -0500

    Rename file as it will now include sets as well

--- tuples&sets.py
+++ tuples&sets.py
@@ -13,3 +13,5 @@ t = (12, 5, 9, 8, 12)
 t.index(1) # returns the index at which a value is found in a tuple
 
 t.count(12) # returns the number of times a value appears in a tuple
+
+##### SETS #####
