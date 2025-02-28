3f1a7816f8bd744474012114ab399d5fe8e53975
chadhindsight <chadrickJ8@gmail.com>
2025-02-28 11:35:18 -0500

    Add more tuple and set related examples

--- tuples&sets.py
+++ tuples&sets.py
@@ -30,3 +30,17 @@ vals.add('Queen Aminata')
 vals.remove('Jordynne Grace') # Will throw an error if the item is not in set
 vals_copy = vals.copy() # Makes a copy of the set
 vals.clear() # remove all items from the set
+
+# 3 - Given the following variable:
+
+values = [10,20,30]
+
+# Create a variable called static_values which is the result of the values variable converted to a tuple
+static_values = tuple(values)
+
+# 4 - Given the following variable
+
+stuff = [1,3,1,5,2,5,1,2,5]
+
+# Create a variable called unique_stuff which is a set of only the unique values in the stuff list
+unique_stuff = set(stuff)
