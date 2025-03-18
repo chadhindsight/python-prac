52c41fb1f7013ab4caad8516da0e1a333efd0657
chadhindsight <chadrickJ8@gmail.com>
2025-03-18 15:21:20 -0400

    Implement a function yell  which accepts a single string argument.  It should return(not print) an uppercased version of the string with an exclamation point aded at the end.  For example:
    yell("go away")   # "GO AWAY!"

--- functions/square_num.py
+++ functions/square_num.py
@@ -2,3 +2,7 @@ def square(num):
     return num * num
 
 print(square(12))
+
+# Implement a function yell  which accepts a single string argument.  It should return(not print) an uppercased version of the string with an exclamation point aded at the end
+def yell(ting):
+    return f"{ting.upper()}!"
