1101399464dd686d947b490e4ef75d4c7689f10c
chadhindsight <chadrickJ8@gmail.com>
2025-03-25 11:34:51 -0400

    Add an example of keyword arguments

--- functions/keyword_args.py
+++ functions/keyword_args.py
@@ -9,3 +9,14 @@ print(exponent(power=3, num=2)) #8
 # Without keywords args, order still matters:
 print(exponent(3,4)) #81
 print(exponent(4,3)) #64
+
+total = 0
+
+def increment():
+	global total #use the global variable total
+	total += 1
+	return total
+
+print(increment()) # 1
+print(increment()) # 2
+print(increment()) # 3
