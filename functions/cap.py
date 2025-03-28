64a8763a1f37a90c7c6a41aab7966243bade025c
chadhindsight <chadrickJ8@gmail.com>
2025-03-28 10:46:34 -0400

    Capitalize the first character in a word

---
+++ functions/cap.py
@@ -0,0 +1,4 @@
+# Slicing the first character (up to index 1) and capitalizing it
+# Adding that to the rest of the string (from index 1 onward)
+def capitalize(string):
+    return string[:1].upper() + string[1:]
