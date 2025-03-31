decb93ebd596d5bf53f271e26b3f6be98035e8ea
chadhindsight <chadrickJ8@gmail.com>
2025-03-31 12:39:06 -0400

    Add a sets solution

--- functions/square_num.py
+++ functions/square_num.py
@@ -3,6 +3,7 @@ def square(num):
 
 print(square(12))
 
-# Implement a function yell  which accepts a single string argument.  It should return(not print) an uppercased version of the string with an exclamation point aded at the end
+# Implement a function yell  which accepts a single string argument.  
+# It should return(not print) an uppercased version of the string with an exclamation point aded at the end
 def yell(ting):
     return f"{ting.upper()}!"
