10923dcd30baba884c3a7a607df80cd22989f4c6
chadhindsight <chadrickJ8@gmail.com>
2025-03-31 12:47:02 -0400

    tings

--- functions/intersection.py
+++ functions/intersection.py
@@ -1,5 +1,4 @@
 # A more Python-ic solution involves using a list comprehension to do the same thing on a single line. Both work just as well.  
-
 def intersection(l1, l2):
     return [val for val in l1 if val in l2]
 
