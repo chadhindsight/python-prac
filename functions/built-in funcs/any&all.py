1345df0e57f7f3102755ad16df0e0fa0dc10fc48
chadhindsight <chadrickJ8@gmail.com>
2025-04-08 16:25:51 -0400

    implement a function is_all_strings  that accepts a single iterable and returns True if it contains ONLY strings.  Otherwise, it should return false.

--- functions/built-in funcs/any&all.py
+++ functions/built-in funcs/any&all.py
@@ -5,3 +5,7 @@ all([num % 2 == 0 for num in nums])
 # Return True if any elements in the iterable are truthy
 nums2 = [2, 6, 12, 9]
 print(any([num % 2 == 0 for num in nums]))
+
+# implement a function is_all_strings  that accepts a single iterable and returns True if it contains ONLY strings.  
+def is_all_strings(strs):
+    return all(type(ting) == str for ting in strs)
