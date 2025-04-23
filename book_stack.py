e7fe60462dc468e61f13265bcd5ccd7952df61d5
chadhindsight <chadrickJ8@gmail.com>
2025-04-23 11:36:50 -0400

    add another lambda example

--- book_stack.py
+++ book_stack.py
@@ -6,10 +6,12 @@ stack_of_books.append("To Kill a Mockingbird")
 stack_of_books.append("Of Mice and Men")
 
 print("Here are the books listed in our stack:", stack_of_books)
+
 # Pop items from the stack (LIFO order) 
 # print(stack_of_books.pop())
 print("The following book was removed from our stack:", stack_of_books.pop()) 
- 
+
+#  "Animal Farm"
 # "Of Mice and Men"
 # "To Kill a Mockingbird"
 # "1984"
