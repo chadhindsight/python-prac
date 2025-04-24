394b0475a5100b74d443223a10fb8d158f79153d
chadhindsight <chadrickJ8@gmail.com>
2025-04-24 13:55:17 -0400

    remove last item added

--- book_stack.py
+++ book_stack.py
@@ -1,5 +1,5 @@
 # Using a list to represent a stack of books
-stack_of_books = ["The Catcher in the Rye", "1984", "To Kill a Mockingbird"]
+stack_of_books = ["The Catcher in the Rye", "1984", "To Kill a Mockingbird", "Of Mice and Men"]
 
 
 # Push items onto the stack
@@ -7,6 +7,6 @@ stack_of_books.append("Of Mice and Men")
 
 # print("Here are the books listed in our stack:", stack_of_books)
 
-# # Pop items from the stack (LIFO order) 
-print("The following book was removed from our stack:", stack_of_books.pop()) 
+ # Pop items from the stack (LIFO order) 
+print(stack_of_books.pop())
 
