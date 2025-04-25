1f931fa5c4095f2cca7988fb3263275c8d498364
chadhindsight <chadrickJ8@gmail.com>
2025-04-25 13:54:46 -0400

    update book example

--- book_stack.py
+++ book_stack.py
@@ -1,12 +1,17 @@
 # Using a list to represent a stack of books
-stack_of_books = ["The Catcher in the Rye", "1984", "To Kill a Mockingbird", "Of Mice and Men"]
+stack_of_books = ["The Catcher in the Rye", "1984", "To Kill a Mockingbird"]
 
 
 # Push items onto the stack
 stack_of_books.append("Of Mice and Men")
 
-# print("Here are the books listed in our stack:", stack_of_books)
 
  # Pop items from the stack (LIFO order) 
 print(stack_of_books.pop())
 
+# STACK REPRESENTED VERTICALLY:
+
+# "Of Mice and Men" 
+# "To Kill a Mockingbird"
+# "1984"
+# "The Catcher in the Rye"
