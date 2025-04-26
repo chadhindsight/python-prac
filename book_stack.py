dda743d51302f51b5854edeaca8acfa983854c69
chadhindsight <chadrickJ8@gmail.com>
2025-04-26 12:04:05 -0400

    stack example

--- book_stack.py
+++ book_stack.py
@@ -4,14 +4,13 @@ stack_of_books = ["The Catcher in the Rye", "1984", "To Kill a Mockingbird"]
 
 # Push items onto the stack
 stack_of_books.append("Of Mice and Men")
-
+# print(stack_of_books)
 
  # Pop items from the stack (LIFO order) 
 print(stack_of_books.pop())
 
 # STACK REPRESENTED VERTICALLY:
 
-# "Of Mice and Men" 
 # "To Kill a Mockingbird"
 # "1984"
 # "The Catcher in the Rye"
