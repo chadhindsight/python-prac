d15944a33d071da96d0233650824470ecd88a54d
chadhindsight <chadrickJ8@gmail.com>
2025-05-01 10:49:53 -0400

    Test error handling by passing in a boolean instead of expected int

--- errors_py/try.py
+++ errors_py/try.py
@@ -8,14 +8,14 @@
 #     print("PROBLEM!")
 # print("after the try")
 
-def get(d,key):
-	try:
-		return d[key]
-	except KeyError:
-		return None
-d = {"name": "Ricky"}
-print(get(d, "city"))
-d["city"]
+# def get(d,key):
+# 	try:
+# 		return d[key]
+# 	except KeyError:
+# 		return None
+# d = {"name": "Ricky"}
+# print(get(d, "city"))
+# d["city"]
 
 
 
