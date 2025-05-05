e1c1a7798546148f0e5c9732dd6ce45e332025af
chadhindsight <chadrickJ8@gmail.com>
2025-05-05 12:11:53 -0400

    Use user input to determine output

--- modulesInPython/ASCIIART.py
+++ modulesInPython/ASCIIART.py
@@ -1,5 +1,9 @@
 import pyfiglet 
+from termcolor import colored
 
 msg = input("What do you want to print?")
-
 color = input("What color?")
+
+art_msg = pyfiglet.figlet_format(msg)
+colored_ascii = colored(art_msg, color=color)
+print(art_msg)
