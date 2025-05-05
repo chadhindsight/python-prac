ba61df769ac42867aa5c707687f24c5c131487fc
chadhindsight <chadrickJ8@gmail.com>
2025-05-05 12:17:46 -0400

    add allowed colors as a tuple

--- modulesInPython/ASCIIART.py
+++ modulesInPython/ASCIIART.py
@@ -4,6 +4,8 @@ from termcolor import colored
 msg = input("What do you want to print? ")
 color = input("What color? ")
 
+valid_colors = ("red", "green", "yellow", "blue", "magenta", "cyan", "white")
+
 art_msg = pyfiglet.figlet_format(msg)
 colored_ascii = colored(art_msg, color=color)
 print(colored_ascii)
