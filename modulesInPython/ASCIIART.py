b07a30f84d1fba416f4f52a29c2a28687c2d6153
chadhindsight <chadrickJ8@gmail.com>
2025-05-05 12:19:20 -0400

    Add a default output if user does not provide a valid number

--- modulesInPython/ASCIIART.py
+++ modulesInPython/ASCIIART.py
@@ -6,6 +6,9 @@ color = input("What color? ")
 
 valid_colors = ("red", "green", "yellow", "blue", "magenta", "cyan", "white")
 
+if color not in valid_colors:
+    color = "magenta"
+    
 art_msg = pyfiglet.figlet_format(msg)
 colored_ascii = colored(art_msg, color=color)
 print(colored_ascii)
