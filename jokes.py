fc32a0bdb8b174add733b0a5bb12d0fae4cbce05
chadhindsight <chadrickJ8@gmail.com>
2025-05-12 10:53:05 -0400

    Add conditional messages

--- jokes.py
+++ jokes.py
@@ -1,10 +1,18 @@
 import requests
-term = input("What would you like to search for? ")
+user_input = input("What would you like to search for? ")
 url = "https://icanhazdadjoke.com/"
 
 response = requests.get(
     url, 
     headers={"Accept": "application/json"},
-    params={"term": term}).json()
+    params={"term": user_input}).json()
 
 print(response)
+
+num_jokes = response["total_jokes"]
+if num_jokes > 1:
+    print("We got mad jokes!")
+elif num_jokes:
+    print("There is one joke only")
+else:
+    print(f"Sorry, could not find a joke with the term {user_input}")    
