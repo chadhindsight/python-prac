1ef4a1b4a35c8e5f5bad8958df68c6616f7a2681
chadhindsight <chadrickJ8@gmail.com>
2025-05-28 11:15:09 -0400

    a single underscore means "private" (sort of)

--- modulesInPython/underscores.py
+++ modulesInPython/underscores.py
@@ -7,7 +7,7 @@ class Person:
 	# Init is a "dunder" method
     def __init__(self):
         self.name = "Tony"
-        # single underscore means "private" (sort of)
+        # a single underscore means "private" (sort of)
         self._secret = "hi!"
         # two leading underscores tells Python to "mangle" the name
         self.__msg = "I like turtles!"
