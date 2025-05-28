7b2c896dfeee7db1178f2857d76f9a78ab062c79
chadhindsight <chadrickJ8@gmail.com>
2025-05-28 11:29:51 -0400

    OOP bank exercise

---
+++ modulesInPython/bofa.py
@@ -0,0 +1,17 @@
+# Define Bank Account Below:
+class BankAccount:
+    def __init__(self, owner, balance =0.0):
+        self.owner = owner
+        self.balance = balance
+    
+    def getBalance(self):
+        return self.balance
+        
+    def deposit(self, num):
+        self.balance += num
+        return self.balance
+    
+    def withdraw(self, num):
+        self.balance -= num
+        return self.balance
+        
