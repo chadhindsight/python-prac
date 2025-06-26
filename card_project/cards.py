73c44aebb57fb656df8f37796b437f4772185fee
chadhindsight <chadrickJ8@gmail.com>
2025-06-26 13:43:00 -0400

    rename file to be more specific

---
+++ card_project/cards.py
@@ -0,0 +1,25 @@
+class Card:
+    def __init__(self, suit, value):
+       self.suit = suit
+       self.value = value 
+    
+    # a getter
+    def __repr__(self):
+        return f"The card selected was the age={self.value} of {self.suit})"
+
+class Deck:
+    def __init__(self):
+        suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+        values = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+        self.cards = []
+
+        
+    
+    def count(self):
+        return f"You have {len(self.cards)} cards in the deck"
+
+
+
+c1 = Card("A", "Spades")
+c1 = Card("5", "Hearts")
+    
