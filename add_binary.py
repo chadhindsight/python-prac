388797d0426d44b34191d3f8d5a2af06f7871254
chadhindsight <chadrickJ8@gmail.com>
2025-08-04 18:14:07 -0400

    Given two binary strings a and b, return their sum as a binary string.

---
+++ add_binary.py
@@ -0,0 +1,21 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        result = ""
+        carry = 0
+        i = len(a) - 1
+        j = len(b) - 1
+
+        while( i >= 0 or j >= 0 or carry > 0):
+            digit_a = int(a[i]) if i >= 0 else 0
+            digit_b = int(b[j]) if j >= 0 else 0
+            
+            total = digit_a + digit_b + carry
+            digit = total % 2
+            carry = total // 2
+
+            result = str(digit) + result
+            
+            i -= 1
+            j -= 1
+        
+        return result
