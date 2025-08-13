c47b5e925faa08d433fb4df3dd5670952bcc380e
chadhindsight <chadrickJ8@gmail.com>
2025-08-13 17:27:21 -0400

    Add a solution using sets

--- functions/intersection.py
+++ functions/intersection.py
@@ -2,6 +2,6 @@
 def intersection(l1, l2):
     return [val for val in l1 if val in l2]
 
-# Sets Solution
+# Solution using sets
 def intersection(list1, list2):
     return [val for val in set(list1) & set(list2)]
