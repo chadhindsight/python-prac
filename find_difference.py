5a9ac56cda96dad0eee6455357c324f509278fb8
chadhindsight <chadrickJ8@gmail.com>
2025-09-05 15:31:26 -0400

    String t is generated by random shuffling string s and then add one more letter at a random position.
    Return the letter that was added to t.

---
+++ find_difference.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        val1 = {}
+        val2 = {}
+
+        for word in s:
+            val1[word] = val1.get(word, 0) + 1
+        
+        for word in t:
+            val2[word] = val2.get(word, 0) + 1
+        
+        for val in val2:
+            if val1.get(val, 0) != val2[val]:
+                return val
