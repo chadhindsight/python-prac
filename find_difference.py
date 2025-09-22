fe6ee980b1bd091a0328ae1d28ad28c744887666
chadhindsight <chadrickJ8@gmail.com>
2025-09-22 10:56:20 -0400

    Change t to g

--- find_difference.py
+++ find_difference.py
@@ -1,12 +1,12 @@
 class Solution:
-    def findTheDifference(self, s: str, t: str) -> str:
+    def findTheDifference(self, s: str, g: str) -> str:
         val1 = {}
         val2 = {}
 
         for word in s:
             val1[word] = val1.get(word, 0) + 1
         
-        for word in t:
+        for word in g:
             val2[word] = val2.get(word, 0) + 1
         
         for value in val2:
