38ae91f6e458b94984eb751b986c2e0943c8f6c3
chadhindsight <chadrickJ8@gmail.com>
2025-09-26 10:28:44 -0400

    Return the minimum cost needed to move all the chips to the same position.

---
+++ coin_cost.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minCostToMoveChips(position):
+        even = odd = 0
+        for num in position:
+            if num % 2 == 0:
+                even += 1
+            else:
+                odd += 1
+        
+        return min(even, odd)
+        
