66d43f1c25d6132483207fd16838c7292e208c10
chadhindsight <chadrickJ8@gmail.com>
2025-10-09 13:52:08 -0400

    Given a positive integer num, return true if num is a perfect square or false otherwise.

---
+++ functions/perfect_sq.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        left = 1
+        right = num
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            squared = mid * mid
+            if squared == num:
+                return True
+            elif squared > num:
+                right = mid - 1
+            elif squared < num:
+                left = mid + 1
+        
+        return False
