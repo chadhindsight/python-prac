4f4615938d79f70479263bb1651ce7daf2448298
chadhindsight <chadrickJ8@gmail.com>
2025-10-13 15:07:04 -0400

    Only invoke function if there are fewer than three positional arguments passed in

--- ensure_fewer_than_three_args.py
+++ ensure_fewer_than_three_args.py
@@ -5,5 +5,5 @@ def ensure_fewer_than_three_args(fn):
     def wrapper(*args, **kwargs):
         if len(args) < 3:
             return fn(*args, **kwargs)
-        return "Too many arguments!"
+        return "Too many damn arguments!"
     return wrapper
