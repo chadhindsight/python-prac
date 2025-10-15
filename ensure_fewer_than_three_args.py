63b60998a9543f7aac20fed6595006b2febc4264
chadhindsight <chadrickJ8@gmail.com>
2025-10-15 13:42:24 -0400

    Given the integer n, return the number of complete rows of the staircase you will build.

--- ensure_fewer_than_three_args.py
+++ ensure_fewer_than_three_args.py
@@ -4,5 +4,5 @@ def ensure_fewer_than_three_args(fn):
     def wrapper(*args, **kwargs):
         if len(args) < 3:
             return fn(*args, **kwargs)
-        return "Too many damn arguments!"
+        return "Too many  arguments!"
     return wrapper
