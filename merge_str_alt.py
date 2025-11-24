838d3e9f8fc7b08bfc1d94a06f24d5096bae8ad3
chadhindsight <chadrickJ8@gmail.com>
2025-11-24 10:33:39 -0500

    You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
    Return the merged string.

--- merge_str_alt.py
+++ merge_str_alt.py
@@ -1,14 +1,14 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-      result = ""
+      result_str = ""
       i = j = 0
 
       while i < len(word1) and j < len(word2):
-        result += word1[i]
-        result += word2[j]
+        result_str += word1[i]
+        result_str += word2[j]
         i += 1
         j += 1
 
-      result += word1[i:]
-      result += word2[j:]
-      return result
+      result_str += word1[i:]
+      result_str += word2[j:]
+      return result_str
