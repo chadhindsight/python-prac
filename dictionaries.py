58b5734fccc944a27b83b28638c74217393938db
chadhindsight <chadrickJ8@gmail.com>
2026-01-14 11:53:47 -0500

    adjust comment

--- dictionaries.py
+++ dictionaries.py
@@ -17,7 +17,7 @@ print(my_fave)
 donations = dict(tam=25.0, lena=88.99, chuck=13.0, linus=99.5, stan=150.0, lisa=50.25, harrison=10.0)
 
 
-# Use a loop to add together all the donations from above and store the resulting number in the variable called donations_total
+# Use a loop to add together all the donations from above and store the resulting number in the variable called 
 donations_total = 0 
 
 for val in donations.values():
