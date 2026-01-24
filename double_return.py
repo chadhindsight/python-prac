7f25518b94b7055eaa9d30f83223d1ceefb6a8dd
chadhindsight <chadrickJ8@gmail.com>
2026-01-24 09:16:04 -0500

    Move double returns decorator exercise to a more relevant location

--- double_return.py
+++
@@ -1,8 +0,0 @@
-from functools import wraps
- 
-def double_return(fn):
-    @wraps(fn)
-    def wrapper(*args, **kwargs):
-        val = fn(*args, **kwargs)
-        return [val, val]
-    return wrapper
