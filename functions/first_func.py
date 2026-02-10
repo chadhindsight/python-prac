fd2d02dec5fba95217d3e6a661b81dd93fac13f4
chadhindsight <chadrickJ8@gmail.com>
2026-02-10 11:50:46 -0500

    Add doc test for make_noise function

--- functions/first_func.py
+++ functions/first_func.py
@@ -1,7 +1,11 @@
 # Define make_noise:
 def make_noise():
-    print("THE CROWD GOES WILD")
+    """
+    >>> make_noise()
+    'THE CROWD GOES WILD'
+    """
+    return 'THE CROWD GOES WILD'
 
 # Execute make_noise:
-make_noise()
+print(make_noise())
 
