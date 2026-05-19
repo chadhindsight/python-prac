948cf769a7f819fa0141d053708c1e950f3dee06
chadhindsight <chadrickJ8@gmail.com>
2026-05-19 12:16:36 -0400

    Explain the use of  both arguments and keyword arguments

--- functions/double_return.py
+++ functions/double_return.py
@@ -2,6 +2,7 @@ from functools import wraps
  
 def double_return(fn):
     @wraps(fn)
+    # this wrapper uses both arguments and keyword arguments
     def wrapper(*args, **kwargs):
         val = fn(*args, **kwargs)
         return [val, val]
