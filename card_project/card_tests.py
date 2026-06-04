921a9cc9e853101eb38d088888bd7f921ae67a7d
chadhindsight <chadrickJ8@gmail.com>
2026-06-04 14:22:15 -0400

    Add card tests, deck specific tests soon come

--- card_project/card_tests.py
+++ card_project/card_tests.py
@@ -1,4 +1,15 @@
 import unittest
 from cards import Card, Deck 
 
-
+class CardTests(unittest.TestCase):
+    def set_up(self):
+        self.card = Card("Hearts", "A")
+    
+    def test_init(self):
+        """cards should have asuit and a value"""
+        self.assertEqual(self.card.suit, "Hearts")
+        self.assertEqual(self.card.value, "A")
+    
+    def test_repr(self):
+        self.assertEqual( repr(self.card),"The card selected is the A of Hearts"
+)
