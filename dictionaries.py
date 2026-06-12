23054f9959ce2275ba4fbce65b61b2b579493ce2
chadhindsight <chadrickJ8@gmail.com>
2026-06-12 18:45:34 -0400

    Write an assertion for the happy path for dealing cards

--- dictionaries.py
+++ dictionaries.py
@@ -29,7 +29,7 @@ print(donations_total)
 "age" in user_info.keys() 
 8 in user_info.values()
 
-# Dictionary methods
+# Some dictionary methods
 inventory = {'croissant': 19, 'bagel': 4, 'muffin': 8, 'cake': 1} #DON'T CHANGE THIS LINE!
 
 # Make a copy of inventory and save it to a variable called stock_list USE A DICTIONARY METHOD
