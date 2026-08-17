abc9e8a1b6465a2b25de497ba4f44dfb94a317a4
chadhindsight <chadrickJ8@gmail.com>
2026-08-17 11:47:10 -0400

    Write a function called copy_and_reverse, which takes in a file name and a new file name and copies the reversed contents of the first file to the second file

--- fileIOSection/copy_and_reverse.py
+++ fileIOSection/copy_and_reverse.py
@@ -1,1 +1,7 @@
 # Write a function called copy_and_reverse, which takes in a file name and a new file name and copies the reversed contents of the first file to the second file.
+def copy_and_reverse(file_name, new_file_name):
+    with open(file_name) as file:
+        text = file.read()
+ 
+    with open(new_file_name, "w") as new_file:
+        new_file.write(text[::-1])
