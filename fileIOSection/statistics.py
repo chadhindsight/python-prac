78e196a14de5458329fc0136a9c33499289abc5a
chadhindsight <chadrickJ8@gmail.com>
2026-08-18 13:46:09 -0400

    Write a function called statistics, which takes in a file name and returns a dictionary with the number of lines, words, and characters in the file.

--- fileIOSection/statistics.py
+++ fileIOSection/statistics.py
@@ -1,3 +1,4 @@
+# Write a function called statistics, which takes in a file name and returns a dictionary with the number of lines, words, and characters in the file.
 def statistics(file_name):
     with open(file_name) as file:
         lines = file.readlines()
