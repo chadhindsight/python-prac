6ca2f5a9dd18fc6da108688ec6a2e4a25380d9d3
chadhindsight <chadrickJ8@gmail.com>
2026-08-24 10:54:29 -0400

    Add example code of how to use the reader module to read CSV files

--- csv-and-python/read-fighters.py
+++ csv-and-python/read-fighters.py
@@ -1,4 +1,7 @@
 from csv import reader 
 
+# Importing and reading data with the reader module
 with open("fighters.csv") as file:
     csv_reader = reader(file)
+    data = list(csv_reader)
+    print(data)
