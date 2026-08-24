1970a812e1b702f68e8dd4076ee3a391be45269e
chadhindsight <chadrickJ8@gmail.com>
2026-08-24 11:01:46 -0400

    Add an example of reading/Parsing CSV Using a DictReader

--- csv-and-python/read-fighters.py
+++ csv-and-python/read-fighters.py
@@ -1,7 +1,18 @@
 from csv import reader 
+from csv import DictReader
 
 # Importing and reading data with the reader module
 with open("fighters.csv") as file:
     csv_reader = reader(file)
+    # NB: data is cast into a list because the csv.reader() function returns an iterable, 
+    # and wrapping it with list() allows you to access all the rows at once
+    # NB: keys are automatically set to be the headers from the given CSV file
     data = list(csv_reader)
     print(data)
+
+# Reading/Parsing CSV Using a DictReader
+with open("fighters.csv") as file:
+    csv_reader = DictReader(file)
+    for row in csv_reader:
+        # Each row is an OrderedDict!
+        print(row['Name']) #Use keys to access data
