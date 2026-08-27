3d433be6f7203932f07d721e4efc58f65206bce8
chadhindsight <chadrickJ8@gmail.com>
2026-08-27 10:07:05 -0400

    Use keys to access data

--- csv-and-python/read-fighters.py
+++ csv-and-python/read-fighters.py
@@ -15,4 +15,4 @@ with open("fighters.csv") as file:
     csv_reader = DictReader(file)
     for row in csv_reader:
         # Each row is an OrderedDict!
-        print(row['Name']) #Use keys to access data
+        print(row['Name'])
