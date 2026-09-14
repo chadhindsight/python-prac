a97cb8add989b443e8a3bbcfdfb136dfa7cefd1b
chadhindsight <chadrickJ8@gmail.com>
2026-09-14 11:19:27 -0400

    Add a comment explaining append mode

--- csv-and-python/add_user.py
+++ csv-and-python/add_user.py
@@ -1,5 +1,6 @@
 from csv import writer;
 
+# The "a" means append mode, lets us add things to the end of the file
 def add_user(first, last):
     with open('users.csv', "a") as file:
         csv_writer = writer(file)
