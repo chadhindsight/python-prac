5f6cf3d6d4f712ebf496dbbb2765af5899b8f72e
chadhindsight <chadrickJ8@gmail.com>
2026-09-25 09:14:12 -0400

    Example of how to unpickle something

--- csv-and-python/pickle/pickling_example.py
+++ csv-and-python/pickle/pickling_example.py
@@ -28,7 +28,7 @@ blue = Cat("Blue", "Scottish Fold", "String")
 with open("pets.pickle", "wb") as file:
 	pickle.dump(blue, file)
 
-# To unpickle something:
+# Example of how to unpickle something:
 # with open("pets.pickle", "rb") as file:
 # 	zombie_blue = pickle.load(file)
 # 	print(zombie_blue)
